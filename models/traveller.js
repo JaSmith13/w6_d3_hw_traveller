b4c0753ef56a4019c0ce13149f148552aa6a2893
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-16 14:09:22 +0100

    passes mvp

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+    return this.journeys.map(journey => journey.startLocation)
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation)
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport)
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance > minDistance)
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let uniqueModes = []
+  this.journeys.forEach(journey => {
+    if (!(uniqueModes.includes(journey.transport))){
+      uniqueModes.push(journey.transport)
+    }
+  });
+  return uniqueModes
+};
+
+
+module.exports = Traveller;
